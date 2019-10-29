1d2299bf313fbdc223b4f0a0eaf8d25cb1784f92
jordanforbes <jordforb@gmail.com>
2019-10-28 22:58:08 -0400

    e1 done

--- e1.js
+++ e1.js
@@ -6,10 +6,28 @@ shortcut = () => {
     console.log(vowels)
 
     for (i = 0; i < foo.length; i++) {
-        vowels.includes(foo[i]) ? foo.slice(0, i)
+        // console.log(foo)
+        // console.log(foo[i])
+        // vowels.includes(foo[i]) ? foo.replace(foo[i], '')
+        if (vowels.includes(foo[i - 1])) {
+            return foo.replace(foo[i - 1], '')
+
+        }
     }
 
     return foo
 }
+
+// function shortcut(str) {
+//   return str.split('').filter(function(e) {
+//     return ['a', 'e', 'i', 'o', 'u'].indexOf(e) == -1 
+//   }).join('')
+// }
+
+//solutions 
+
+// function shortcut(string){
+//   return string.replace(/[aeiou]/g,'')
+// }
 
 console.log(shortcut())
